1f77129b072e13238388377b03663c6e2258d463
cassylanza <cassalanza@gmail.com>
2025-12-17 12:41:36 -0500

    readjusted layout a little, still needs more changing

--- frontend/src/components/home/FunctionCards.tsx
+++ frontend/src/components/home/FunctionCards.tsx
@@ -2,29 +2,37 @@ import { SquarePen, Compass, HandHeart } from 'lucide-react';
 
 const FunctionCards = () => {
     return (
-        <div className="flex h-[calc(100vh-5rem)] grid grid-rows-3 mr-10 ml-10">
-            <article className="bg-white row-span-1 rounded-xl p-3 flex flex-row mb-3 drop-shadow-md">
-                <SquarePen className="h-45 w-45 text-text-green mr-4 flex-shrink-0 mb-4 mt-4 drop-shadow-md" />
-                <div className="flex-1">
-                    <p className="flex text-yellow-950 text-5xl mt-7 ml-2 drop-shadow-sm"> Post Offerings </p>
-                    <p className="flex text-yellow-950 text-4xl mt-4 ml-2 drop-shadow-sm"> Share the resources and services you can provide. </p>
-                </div>
-            </article>
-            <article className="bg-white row-start-2 rounded-xl p-3 flex flex-row mb-3 drop-shadow-md">
-                <Compass className="h-45 w-45 text-text-green mr-4 flex-shrink-0 mb-4 mt-4 drop-shadow-md" />
-                <div className="flex-1">
-                    <p className="flex text-yellow-950 text-5xl mt-7 ml-2 drop-shadow-sm"> Discover Help </p>
-                    <p className="flex text-yellow-950 text-4xl mt-4 ml-2 drop-shadow-sm"> Browse verified local organizations and posts. </p>
-                </div>
-            </article>
-            <article className="bg-white row-start-3 rounded-xl p-3 flex flex-row mb-6 drop-shadow-md">
-                <HandHeart className="h-45 w-45 text-text-green mr-4 flex-shrink-0 mb-4 mt-4 drop-shadow-md" />
-                <div className="flex-1">
-                    <p className="flex text-yellow-950 text-5xl mt-7 ml-2 drop-shadow-sm"> Donate Service </p>
-                    <p className="flex text-yellow-950 text-4xl mt-4 ml-2 drop-shadow-sm"> Volunteer your time and talent to the community. </p>
-                </div>
-            </article>
-        </div>
+        <main className="flex h-fit">
+            <div className="grid grid-rows-3">
+                <section className="row-span-1 flex flex-row px-7 px-4 mt-1">
+                    <article className="bg-white flex rounded-xl p-3 mb-6 drop-shadow-md">
+                        <SquarePen className="h-40 w-40 text-text-green flex-shrink-0 mt-4 ml-4 mb-6 drop-shadow-md" />
+                        <div className="flex-1 ml-10 mr-4">
+                            <p className="flex text-yellow-950 text-5xl mt-7 drop-shadow-sm"> Post Offerings </p>
+                            <p className="flex text-yellow-950 text-4xl mt-4 drop-shadow-sm"> Share the resources and services you can provide. </p>
+                        </div>
+                    </article>
+                </section>
+                <section className="row-start-2 flex flex-row px-7 px-4">
+                    <article className="bg-white row-start-2 rounded-xl p-3 flex flex-row mb-6 drop-shadow-md">
+                        <Compass className="h-40 w-40 text-text-green flex-shrink-0 mt-4 ml-4 mb-6 drop-shadow-md" />
+                        <div className="flex-1 ml-10 mr-4">
+                            <p className="flex text-yellow-950 text-5xl mt-7 drop-shadow-sm"> Discover Help </p>
+                            <p className="flex text-yellow-950 text-4xl mt-4 drop-shadow-sm"> Browse verified local organizations and posts. </p>
+                        </div>
+                    </article>
+                </section>
+                <section className="row-span-1 flex flex-row px-7 px-4">
+                    <article className="bg-white row-start-3 rounded-xl p-3 flex flex-row mb-6 drop-shadow-md">
+                        <HandHeart className="h-40 w-40 text-text-green flex-shrink-0 mt-4 ml-4 mb-6 drop-shadow-md" />
+                        <div className="flex-1 ml-10 mr-4">
+                            <p className="flex text-yellow-950 text-5xl mt-7 drop-shadow-sm"> Donate Service </p>
+                            <p className="flex text-yellow-950 text-4xl mt-4 drop-shadow-sm"> Volunteer your time and talent to the community. </p>
+                        </div>
+                    </article>
+                </section>
+            </div>
+        </main>
     );
 };
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -8,12 +8,12 @@ const SVC_Basilica = new URL('../assets/SVC_Basilica.jpg', import.meta.url).href
 const HomePage = () => {
   return (
   <main className="flex min-h-[calc(100vh-5rem)]">
-    <div className="flex">
+    <div className="flex px-7">
       <div className="grid grid-rows-2 h-[calc(100vh-5rem)]">
-          <section className="row-span-1 flex flex-row flex-1 px-7 py-4 justify-center">
+          <section className="row-span-1 flex flex-row flex-1 py-4 justify-center">
             <img src={SVC_Basilica} alt="SVC Basilica" className="w-full h-auto object-cover rounded-xl drop-shadow-md" />
           </section>
-          <section className="row-start-2 flex flex-row flex-1 px-7 py-4">
+          <section className="row-start-2 flex flex-row flex-1 py-4">
             <SellingPoints />
           </section>
       </div>
